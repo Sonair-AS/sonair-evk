7d270190ba27a4dbb6d329c2559fcd5a9105aada
Endre Bakka <endre.bakka@sonair.com>
2024-10-18 11:29:28 +0200

    Add missing import

--- ros/sonair_evk/sonair_evk/convert_coap_payload_to_pointcloud.py
+++ ros/sonair_evk/sonair_evk/convert_coap_payload_to_pointcloud.py
@@ -1,6 +1,7 @@
 # Copyright (C), 2024, Sonair AS
 #
 # This software is free software, licensed according to the MIT License:https://en.wikipedia.org/wiki/MIT_License import struct
+import struct
 from typing import Tuple
 
 from .sonair_evk_status import SonairEvkStatus
